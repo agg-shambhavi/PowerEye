972e831ab62e738f99dcd582475bd2aec2d2b34c
Shambhavi Aggarwal <agg.shambhavi@gmail.com>
2020-12-25 13:23:14 +0530

    refactor code

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import dlib
+from math import hypot
 
 cap = cv2.VideoCapture(0)
 
@@ -32,10 +33,8 @@ while True:
         le_center_top = midpoint(landmarks.part(37), landmarks.part(38))
         le_center_bottom = midpoint(landmarks.part(41), landmarks.part(40))
 
-        le_hor_line = cv2.line(frame, le_left_point,
-                               le_right_point, (0, 255, 0), 1)
-        le_ver_line = cv2.line(frame, le_center_top,
-                               le_center_bottom, (0, 255, 0), 2)
+        #le_hor_line = cv2.line(frame, le_left_point,le_right_point, (0, 255, 0), 1)
+        #le_ver_line = cv2.line(frame, le_center_top,le_center_bottom, (0, 255, 0), 2)
 
         # re stands for right eye
         # re coordinates and lines
@@ -44,10 +43,8 @@ while True:
         re_center_top = midpoint(landmarks.part(43), landmarks.part(44))
         re_center_bottom = midpoint(landmarks.part(47), landmarks.part(46))
 
-        re_hor_line = cv2.line(frame, re_left_point,
-                               re_right_point, (0, 255, 0), 1)
-        re_ver_line = cv2.line(frame, re_center_top,
-                               re_center_bottom, (0, 255, 0), 2)
+        #re_hor_line = cv2.line(frame, re_left_point,re_right_point, (0, 255, 0), 1)
+        #re_ver_line = cv2.line(frame, re_center_top,re_center_bottom, (0, 255, 0), 2)
 
         # gaze detection
         left_eye_region = np.array([(landmarks.part(36).x, landmarks.part(36).y),
@@ -55,11 +52,30 @@ while True:
                                     (landmarks.part(38).x, landmarks.part(38).y),
                                     (landmarks.part(39).x, landmarks.part(39).y),
                                     (landmarks.part(40).x, landmarks.part(40).y),
-                                    (landmarks.part(41).x, landmarks.part(41).y], np.int32)
+                                    (landmarks.part(41).x, landmarks.part(41).y)], np.int32)
+
+        right_eye_region = np.array([(landmarks.part(42).x, landmarks.part(42).y),
+                                     (landmarks.part(43).x, landmarks.part(43).y),
+                                     (landmarks.part(44).x, landmarks.part(44).y),
+                                     (landmarks.part(45).x, landmarks.part(45).y),
+                                     (landmarks.part(46).x, landmarks.part(46).y),
+                                     (landmarks.part(47).x, landmarks.part(47).y)], np.int32)
+
+        cv2.polylines(frame, [left_eye_region], True, (0, 0, 255), 2)
+        cv2.polylines(frame, [right_eye_region], True, (0, 0, 255), 2)
+
+        min_x = np.min(left_eye_region[:, 0])
+        max_x = np.max(left_eye_region[:, 0])
+        min_y = np.min(left_eye_region[0, :])
+        max_y = np.max(left_eye_region[0, :])
+
+        #eye = frame[min_y:max_y, min_x:max_x]
+        #eye = cv2.resize(eye, None, fx=5, fy=5)
+        #cv2.imshow("Eye", eye)
 
     cv2.imshow("Frame", frame)
 
-    key=cv2.waitKey(1)
+    key = cv2.waitKey(1)
     if key == 27:
         break
 
